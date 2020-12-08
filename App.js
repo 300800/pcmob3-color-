a9540763563968d1f46307bfdbd25422b3ceba39
grace <gracechongbl@gmail.com>
2020-12-08 12:12:22 +0800

    details screen and kueh lapis screen

--- App.js
+++ App.js
@@ -1,18 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
+import { FlatList } from "react-native-gesture-handler";
 
-function HomeScreen() {
+//constant COLORS = [
+//{red: 255, green: 128, blue: 0, id: "0"},
+//{green: 0, green: 128, blue: 255, id: "1"},
+//{blue: 255, green: 128, blue: 0, id: "2"},
+//];
+
+function HomeScreen({ navigation }) {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 128, blue: 128, id: "0" },
+    { green: 0, red: 128, blue: 255, id: "1" },
+    { blue: 128, red: 0, green: 255, id: "2" },
+  ]);
+
+  function DetailsScreen() {
+    return <Text> Hey these are my details</Text>;
+  }
+
+  function renderItem({ item }) {
+    //return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+    return (
+      <TouchableOpacity onPress={() => navigation.navigate("DetailsScreen")}>
+        <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+      </TouchableOpacity>
+    );
+  }
+
+  function addColor() {
+    let newColor = {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: colorArray.length.toString(),
+    };
+    setColorArray([...colorArray, newColor]);
+  }
+
+  function resetColor() {
+    setColorArray([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>pcmob3-2</Text>
-      <StatusBar style="auto" />
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "red" }}>Add colour</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={resetColor}
+      ></TouchableOpacity>
+
+      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
 }
@@ -23,7 +71,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Colors" component={HomeScreen} />
+        <Stack.Screen name="Kueh Lapis" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -35,5 +84,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  list: {
+    width: "100%",
   },
 });
